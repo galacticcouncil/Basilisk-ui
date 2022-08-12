0059619905e5bf2183beb702710cf8c681637c9e
Duong Tat Dat <david@duong.cz>
2022-08-12 06:32:38 +0200

    chore: fix prettier

--- src/components/Page/Header/MenuList/MenuList.tsx
+++ src/components/Page/Header/MenuList/MenuList.tsx
@@ -1,15 +1,15 @@
-import { Box } from "components/Box/Box";
-import { FC } from "react";
-import { MenuItem, MenuItemProps } from "../MenuItem/MenuItem";
+import { Box } from "components/Box/Box"
+import { FC } from "react"
+import { MenuItem, MenuItemProps } from "../MenuItem/MenuItem"
 
 type MenuListProps = {
-  items: MenuItemProps[];
-};
+  items: MenuItemProps[]
+}
 
 export const MenuList: FC<MenuListProps> = ({ items }) => (
   <Box flex gap={30}>
-    {items.map((item) => (
+    {items.map(item => (
       <MenuItem text={item.text} active={item.active} />
     ))}
   </Box>
-);
+)
