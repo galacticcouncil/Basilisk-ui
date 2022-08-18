939142b717873a193840a731487d78da81b46a7a
obsivac <filip.obsivac@cleevio.com>
2022-08-18 10:28:57 +0200

    HYDRA-34: added farms modal without deposit input

---
+++ src/components/FillBar/FillBar.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react"
+import { StyledBar, StyledFill } from "components/FillBar/FillBar.styled"
+
+type Props = {
+  percentage: number
+}
+
+export const FillBar: FC<Props> = ({ percentage }) => {
+  return (
+    <StyledBar>
+      <StyledFill width={percentage} />
+    </StyledBar>
+  )
+}
