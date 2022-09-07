08a4bcef751a9af37712ed7e9f07cb9178379578
tomashapl <tomas.hapl@cleevio.com>
2022-09-07 14:08:44 +0200

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import svgr from "vite-plugin-svgr"
-import wasm from "vite-plugin-wasm";
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
@@ -28,7 +27,6 @@ export default defineConfig({
         ],
       },
     }),
-    wasm(),
     svgr(),
   ],
 })
