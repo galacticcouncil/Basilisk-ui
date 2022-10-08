11df63ebeca0ec2f9785113a4f327f1f926c2afe
Duong Tat Dat <david@duong.cz>
2022-10-08 02:41:15 +0200

    Fix jsx import for recharts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
   build: {
     target: "esnext",
     outDir: "build",
+    rollupOptions: {
+      makeAbsoluteExternalsRelative: true,
+    },
   },
   optimizeDeps: {
     esbuildOptions: {
@@ -21,7 +24,7 @@ export default defineConfig({
   plugins: [
     tsconfigPaths(),
     react({
-      jsxImportSource: "/src/jsx",
+      jsxImportSource: "@basilisk/jsx",
       babel: {
         plugins: ["@emotion/babel-plugin"],
       },
