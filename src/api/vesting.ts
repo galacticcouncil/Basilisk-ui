3fa6898366a055c922a445e5c7060d811af600b2
tomashapl <tomas.hapl@cleevio.com>
2022-10-21 15:00:27 +0200

    feat(HYDRA-165): Claimable balance

---
+++ src/api/vesting.ts
@@ -0,0 +1,100 @@
+import { AccountId32 } from "@polkadot/types/interfaces"
+import { useApiPromise } from "../utils/network"
+import { useQuery } from "@tanstack/react-query"
+import { QUERY_KEYS } from "../utils/queryKeys"
+import { undefinedNoop } from "../utils/helpers"
+import { ApiPromise } from "@polkadot/api"
+import { Maybe } from "../utils/types"
+import { useBestNumber } from "./chain"
+import { u32 } from "@polkadot/types"
+import BigNumber from "bignumber.js"
+import { useAccountStore } from "../state/store"
+import { BN_0 } from "../utils/constants"
+
+export const useVestingSchedules = (address: Maybe<AccountId32 | string>) => {
+  const api = useApiPromise()
+  return useQuery(
+    QUERY_KEYS.vestingSchedules(address),
+    address != null ? getVestingSchedules(api, address) : undefinedNoop,
+    { enabled: !!address },
+  )
+}
+
+export const useVestingLockBalance = (address: Maybe<AccountId32 | string>) => {
+  const api = useApiPromise()
+  return useQuery(
+    QUERY_KEYS.vestingLockBalance(address),
+    address != null ? getVestingLockBalance(api, address) : undefinedNoop,
+  )
+}
+
+const getVestingLockBalance =
+  (api: ApiPromise, address: AccountId32 | string) => async () => {
+    const data = await api.query.balances.locks(address)
+    return (
+      data
+        .find((lock) => lock.id.toString() === "ormlvest")
+        ?.amount.toBigNumber() ?? BN_0
+    )
+  }
+
+export const getVestingSchedules =
+  (api: ApiPromise, address: AccountId32 | string) => async () => {
+    const data = await api.query.vesting.vestingSchedules(address)
+    return data.map((value) => {
+      return {
+        start: value.start,
+        period: value.period,
+        periodCount: value.periodCount,
+        perPeriod: new BigNumber(value.perPeriod.toHex()),
+      }
+    })
+  }
+
+export type ScheduleType = Awaited<
+  ReturnType<ReturnType<typeof getVestingSchedules>>
+>[number]
+
+const getScheduleClaimableBalance = (
+  schedule: ScheduleType,
+  blockNumber: u32,
+  lockedBalance: BigNumber,
+) => {
+  const start = schedule.start.toBigNumber()
+  const period = schedule.period.toBigNumber()
+  const periodCount = schedule.periodCount.toBigNumber()
+  const blockNumberAsBigNumber = blockNumber.toBigNumber()
+
+  const numOfPeriods = blockNumberAsBigNumber.minus(start).div(period)
+  const vestedOverPeriods = numOfPeriods.times(schedule.perPeriod)
+  const originalLock = periodCount.times(schedule.perPeriod)
+  const futureLock = originalLock.minus(vestedOverPeriods)
+
+  return lockedBalance.minus(futureLock)
+}
+
+export const useVestingClaimableBalance = () => {
+  const { account } = useAccountStore()
+  const vestingSchedulesQuery = useVestingSchedules(account?.address)
+  const vestingLockBalanceQuery = useVestingLockBalance(account?.address)
+  const bestNumberQuery = useBestNumber()
+
+  if (
+    vestingSchedulesQuery.data &&
+    bestNumberQuery.data &&
+    vestingLockBalanceQuery.data
+  ) {
+    return vestingSchedulesQuery.data.reduce((acc, cur) => {
+      acc.plus(
+        getScheduleClaimableBalance(
+          cur,
+          bestNumberQuery.data.relaychainBlockNumber,
+          vestingLockBalanceQuery.data ?? BN_0,
+        ),
+      )
+      return acc
+    }, BN_0)
+  }
+
+  return BN_0
+}
--- src/sections/wallet/vesting/WalletVestingHeader.tsx
+++ src/sections/wallet/vesting/WalletVestingHeader.tsx
@@ -2,9 +2,26 @@ import { Text } from "../../../components/Typography/Text/Text"
 import { Heading } from "../../../components/Typography/Heading/Heading"
 import { Separator } from "../../../components/Separator/Separator"
 import { useTranslation } from "react-i18next"
+import { useVestingClaimableBalance } from "../../../api/vesting"
+import { useSpotPrice } from "../../../api/spotPrice"
+import { useAUSD } from "../../../api/asset"
+import { NATIVE_ASSET_ID } from "../../../utils/network"
+import { useMemo } from "react"
+import { BN_0 } from "../../../utils/constants"
 
 export const WalletVestingHeader = () => {
   const { t } = useTranslation()
+
+  const claimableBalance = useVestingClaimableBalance()
+  const AUSD = useAUSD()
+  const spotPrice = useSpotPrice(NATIVE_ASSET_ID, AUSD.data?.id)
+
+  const claimableUSD = useMemo(() => {
+    if (claimableBalance && spotPrice.data) {
+      return claimableBalance.times(spotPrice.data.spotPrice)
+    }
+    return BN_0
+  }, [claimableBalance, spotPrice])
 
   return (
     <div
@@ -17,9 +34,18 @@ export const WalletVestingHeader = () => {
         </Text>
         <div sx={{ flex: "row", align: "baseline" }}>
           <Heading as="h3" sx={{ fontSize: [16, 42], fontWeight: 900 }}>
-            {t("value.usd", { amount: "1" })}
+            {t("value", { value: claimableBalance, decimalPlaces: 2 })}
           </Heading>
         </div>
+        <Text
+          sx={{
+            mt: 10,
+          }}
+          fs={16}
+          lh={18}
+        >
+          {t("value.usd", { amount: claimableUSD })}
+        </Text>
       </div>
       <Separator sx={{ mb: 12, display: ["inherit", "none"] }} />
       <div sx={{ flex: ["row", "column"], justify: "space-between" }}>
