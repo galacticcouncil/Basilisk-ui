476103da03ee492d105a9769cccae6472728a229
Duong Tat Dat <david@duong.cz>
2022-11-10 13:18:22 +0100

    chore: code review

--- src/sections/wallet/transfer/crosschain/WalletTransferSectionCrosschain.utils.tsx
+++ src/sections/wallet/transfer/crosschain/WalletTransferSectionCrosschain.utils.tsx
@@ -3,6 +3,32 @@ import { ReactComponent as Wormhole } from "assets/icons/crosschains/Wormhole.sv
 import { ReactComponent as Karura } from "assets/icons/crosschains/Karura.svg"
 import { ReactNode } from "react"
 import { ExternalLink } from "components/Link/ExternalLink"
+import { Trans, useTranslation } from "react-i18next"
+
+function CrosschainKaruraGuide() {
+  const { t } = useTranslation()
+
+  return (
+    <div sx={{ flex: "column", align: "center", gap: 16 }}>
+      <Trans t={t} i18nKey="wallet.assets.transfer.crosschain.guide.karura">
+        <Text tAlign="center" fw={400} sx={{ maxWidth: 400 }} />
+
+        <Text tAlign="center" fw={400} sx={{ maxWidth: 400 }} />
+
+        <Text tAlign="center" fw={400} sx={{ maxWidth: 400 }} />
+
+        <Text tAlign="center" fw={400} sx={{ color: "yellow300" }} />
+
+        <Text tAlign="center" fw={400} sx={{ maxWidth: 500 }} />
+
+        <ExternalLink
+          href="https://docs.bsx.fi/howto_bridge/#karura"
+          sx={{ color: "primary450" }}
+        />
+      </Trans>
+    </div>
+  )
+}
 
 export const CROSSCHAINS: Array<{
   icon: ReactNode
@@ -16,38 +42,7 @@ export const CROSSCHAINS: Array<{
     name: "Karura",
     type: "both",
     url: "https://apps.karura.network/bridge",
-    guide: (
-      <div sx={{ flex: "column", align: "center", gap: 16 }}>
-        <Text tAlign="center" fw={400} sx={{ maxWidth: 400 }}>
-          1. First you will have to use the Bridge App to move them from
-          Kusama's chain to Karura's
-        </Text>
-
-        <Text tAlign="center" fw={400} sx={{ maxWidth: 400 }}>
-          2. Once your KSM are in Karura's network.
-        </Text>
-
-        <Text tAlign="center" fw={400} sx={{ maxWidth: 400 }}>
-          3. You should be able to trade them in Basilisk.
-        </Text>
-
-        <Text tAlign="center" fw={400} sx={{ color: "yellow300" }}>
-          IMPORTANT: Any assets must first be sent to Kusama, then to Karura.
-        </Text>
-
-        <Text tAlign="center" fw={400} sx={{ maxWidth: 500 }}>
-          Below you will find step by step instructions on how to move your KSM
-          from Karura to Kusama.
-        </Text>
-
-        <ExternalLink
-          href="https://docs.bsx.fi/howto_bridge/#karura"
-          sx={{ color: "primary450" }}
-        >
-          Go to full instructions
-        </ExternalLink>
-      </div>
-    ),
+    guide: <CrosschainKaruraGuide />,
   },
   {
     icon: <Wormhole />,
