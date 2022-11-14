2c8120406480d811bdf353dd597e6f3f8f562c90
tomashapl <tomas.hapl@cleevio.com>
2022-11-14 14:20:12 +0100

    fix: Remove claimable in header

--- src/sections/wallet/vesting/WalletVestingHeader.tsx
+++ src/sections/wallet/vesting/WalletVestingHeader.tsx
@@ -1,12 +1,7 @@
 import { Text } from "components/Typography/Text/Text"
 import { Heading } from "components/Typography/Heading/Heading"
 import { Trans, useTranslation } from "react-i18next"
-import { Separator } from "../../../components/Separator/Separator"
-import {
-  useVestingTotalClaimableBalance,
-  useVestingScheduleEnd,
-  useVestingTotalVestedAmount,
-} from "api/vesting"
+import { useVestingScheduleEnd, useVestingTotalVestedAmount } from "api/vesting"
 import { useSpotPrice } from "api/spotPrice"
 import { useAUSD } from "api/asset"
 import { useMemo } from "react"
@@ -22,7 +17,6 @@ import { separateBalance } from "../../../utils/balance"
 export const WalletVestingHeader = () => {
   const { t } = useTranslation()
 
-  const { data: claimableBalanceAmount } = useVestingTotalClaimableBalance()
   const { data: totalVestedAmount } = useVestingTotalVestedAmount()
   const { data: vestingScheduleEnd } = useVestingScheduleEnd()
 
@@ -30,15 +24,7 @@ export const WalletVestingHeader = () => {
   const spotPrice = useSpotPrice(NATIVE_ASSET_ID, AUSD.data?.id)
   const { data: nativeAsset } = useAssetMeta(NATIVE_ASSET_ID)
 
-  const claimableValue = claimableBalanceAmount ?? BN_0
   const totalVestedValue = totalVestedAmount ?? BN_0
-
-  const claimableUSD = useMemo(() => {
-    if (claimableValue && spotPrice.data) {
-      return claimableValue.times(spotPrice.data.spotPrice)
-    }
-    return null
-  }, [claimableValue, spotPrice])
 
   const totalVestedUSD = useMemo(() => {
     if (totalVestedValue && spotPrice.data) {
@@ -52,45 +38,6 @@ export const WalletVestingHeader = () => {
       sx={{ flex: ["column", "row"], mb: 40 }}
       css={{ "> *:not([role='separator'])": { flex: 1 } }}
     >
-      <div sx={{ flex: ["row", "column"], justify: "space-between" }}>
-        <Text color="neutralGray300" sx={{ mb: 14 }}>
-          {t("wallet.vesting.claimable")}
-        </Text>
-
-        {/* TODO: add skeleton loader */}
-        <div sx={{ flex: "row", align: "start" }}>
-          <Heading as="h3" sx={{ fontSize: [16, 58], fontWeight: 900 }}>
-            <Trans
-              t={t}
-              i18nKey="wallet.vesting.claimable.value"
-              tOptions={{
-                ...separateBalance(claimableValue, {
-                  fixedPointScale: 12,
-                  decimalPlaces: 2,
-                }),
-              }}
-            >
-              <span
-                css={css`
-                  color: rgba(${theme.rgbColors.white}, 0.4);
-                  font-size: 32px;
-                `}
-              />
-            </Trans>
-          </Heading>
-        </div>
-        <Text
-          sx={{
-            mt: 10,
-          }}
-          color="neutralGray300"
-          fs={16}
-          lh={18}
-        >
-          {t("value.usd", { amount: claimableUSD })}
-        </Text>
-      </div>
-      <Separator sx={{ mb: 12, display: ["inherit", "none"] }} />
       <div sx={{ flex: ["row", "column"], justify: ["start", "center"] }}>
         <Text color="neutralGray300" sx={{ mb: 14 }}>
           {t("wallet.vesting.total_vested")}
