3c0157e206ebac7666fca4d5b1585ae91b1f16ee
Duong Tat Dat <david@duong.cz>
2022-11-16 18:47:16 +0100

    Fix lint

--- src/sections/pools/pool/modals/addLiquidity/PoolAddLiquidityModal.tsx
+++ src/sections/pools/pool/modals/addLiquidity/PoolAddLiquidityModal.tsx
@@ -1,7 +1,7 @@
 import { PoolAddLiquidityAssetSelect } from "./assetSelect/PoolAddLiquidityAssetSelect"
 import { getAssetLogo } from "components/AssetIcon/AssetIcon"
 import { PoolAddLiquidityConversion } from "./conversion/PoolAddLiquidityConversion"
-import { BN_1, BN_100, DEFAULT_DECIMALS } from "utils/constants"
+import { BN_1, DEFAULT_DECIMALS } from "utils/constants"
 import { Row } from "components/Row/Row"
 import { Separator } from "components/Separator/Separator"
 import { Text } from "components/Typography/Text/Text"
