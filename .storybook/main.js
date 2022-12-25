3666819d87624fce4f70b15f44adc5d0746659c1
David Duong <david@duong.cz>
2022-12-25 12:08:20 +0100

    Upgrade to Storybook 7.0.0-beta.15 (#230)
    * Upgrade to Storybook 7.0.0-beta.15
    
    * Temporarily use deprecated branch
    
    * Upgrade lockfile

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,3 @@
-const { mergeConfig } = require("vite")
-const { default: tsconfigPaths } = require("vite-tsconfig-paths")
-const svgr = require("vite-plugin-svgr")
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -9,13 +5,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-vite",
-  },
-  async viteFinal(config) {
-    return mergeConfig(config, {
-      plugins: [tsconfigPaths(), svgr()],
-    })
+  framework: {
+    name: "@storybook/react-vite",
   },
 }
