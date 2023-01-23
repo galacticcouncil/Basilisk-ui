89c1c1505a9ca636e9e41cceffa7c5bd32f1982e
Filip Obsivac <filip.obsivac@cleevio.com>
2023-01-23 14:32:07 +0100

    CR fixes

--- src/sections/wallet/assets/table/data/WalletLiquidityPositionsData.tsx
+++ src/sections/wallet/assets/table/data/WalletLiquidityPositionsData.tsx
@@ -1,18 +1,29 @@
-import { DualAssetIcons } from "../../../../../components/DualAssetIcons/DualAssetIcons"
-import { getAssetLogo } from "../../../../../components/AssetIcon/AssetIcon"
+import { DualAssetIcons } from "components/DualAssetIcons/DualAssetIcons"
+import { getAssetLogo, getAssetName } from "components/AssetIcon/AssetIcon"
+import { Text } from "components/Typography/Text/Text"
 
 export const WalletLiquidityPositionsTableName = (props: {
   symbolA: string
   symbolB: string
 }) => {
   return (
-    <DualAssetIcons
-      firstIcon={{
-        icon: getAssetLogo(props.symbolA),
-      }}
-      secondIcon={{
-        icon: getAssetLogo(props.symbolB),
-      }}
-    />
+    <div sx={{ flex: "row", align: "center", gap: 8 }}>
+      <DualAssetIcons
+        firstIcon={{
+          icon: getAssetLogo(props.symbolA),
+        }}
+        secondIcon={{
+          icon: getAssetLogo(props.symbolB),
+        }}
+      />
+      <div sx={{ display: ["block", "none"] }}>
+        <Text fw={600} fs={12} lh={16} color="white">
+          {props.symbolA}/{props.symbolB}
+        </Text>
+        <Text fw={500} fs={11} lh={14} color="neutralGray400">
+          {getAssetName(props.symbolA)}/{getAssetName(props.symbolB)}
+        </Text>
+      </div>
+    </div>
   )
 }
