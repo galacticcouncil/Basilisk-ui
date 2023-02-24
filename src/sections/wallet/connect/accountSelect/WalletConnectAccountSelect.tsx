d100f6c3e779b8c6dba03e8c748ac0c69dd775ca
Viacheslav Kulinich <viacheslav.kulinich@cleevio.com>
2023-02-24 13:59:42 +0100

    Sorted selected account

--- src/sections/wallet/connect/accountSelect/WalletConnectAccountSelect.tsx
+++ src/sections/wallet/connect/accountSelect/WalletConnectAccountSelect.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import { FC, ReactNode } from "react"
 import { Text } from "components/Typography/Text/Text"
 import { useTranslation } from "react-i18next"
 import { useQuery } from "@tanstack/react-query"
@@ -25,38 +25,46 @@ export const WalletConnectAccountSelect: FC<Props> = ({
     return await wallet?.getAccounts()
   })
 
+  const accountComponents = accounts.data?.reduce((memo, account) => {
+    const accountName = account.name ?? account.address
+    // As Talisman allows Ethereum accounts to be added as well, filter these accounts out
+    // as I believe these are not supported on Basilisk / HydraDX
+    // @ts-expect-error
+    if (account.type !== "ethereum" && account.type !== "ecdsa") {
+      const isActive = currentAddress === account.address
+      const accountComponent = (
+        <WalletConnectAccountSelectItem
+          isActive={isActive}
+          provider={provider}
+          key={account.address}
+          name={accountName}
+          address={account.address}
+          setAccount={() => {
+            onSelect({
+              name: accountName,
+              address: account.address,
+              provider,
+            })
+          }}
+        />
+      )
+      if (isActive) {
+        memo.unshift(accountComponent)
+      } else {
+        memo.push(accountComponent)
+      }
+    }
+
+    return memo
+  }, [] as ReactNode[])
+
   return (
     <>
       <Text fw={500} color="neutralGray200" sx={{ mt: 6 }}>
         {t("walletConnect.accountSelect.description")}
       </Text>
 
-      <SContainer>
-        {accounts.data
-          // As Talisman allows Ethereum accounts to be added as well, filter these accounts out
-          // as I believe these are not supported on Basilisk / HydraDX
-          // @ts-expect-error
-          ?.filter((i) => i.type !== "ethereum" && i.type !== "ecdsa")
-          ?.map((account) => {
-            const accountName = account.name ?? account.address
-            return (
-              <WalletConnectAccountSelectItem
-                isActive={currentAddress === account.address}
-                provider={provider}
-                key={account.address}
-                name={accountName}
-                address={account.address}
-                setAccount={() => {
-                  onSelect({
-                    name: accountName,
-                    address: account.address,
-                    provider,
-                  })
-                }}
-              />
-            )
-          })}
-      </SContainer>
+      <SContainer>{accountComponents}</SContainer>
     </>
   )
 }
