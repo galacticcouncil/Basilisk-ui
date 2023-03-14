54881d328db85e0782aeb1962189222ad27c7479
vkulinich-cl <115090120+vkulinich-cl@users.noreply.github.com>
2023-03-14 11:00:23 +0100

    Added Uknown toast type (#364)

--- src/sections/transaction/ReviewTransactionToast.tsx
+++ src/sections/transaction/ReviewTransactionToast.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next"
 import { UseMutationResult } from "@tanstack/react-query"
 import { useToast } from "state/toasts"
 import { ToastMessage } from "state/store"
+import { UnknownTransactionState } from "./ReviewTransaction.utils"
 
 export function ReviewTransactionToast<
   TData = unknown,
@@ -19,7 +20,7 @@ export function ReviewTransactionToast<
 }) {
   const toast = useToast()
   const { t } = useTranslation()
-  const { isError, isSuccess, isLoading } = props.mutation
+  const { isError, isSuccess, isLoading, error } = props.mutation
 
   const toastRef = useRef<typeof toast>(toast)
   useEffect(() => void (toastRef.current = toast), [toast])
@@ -47,11 +48,19 @@ export function ReviewTransactionToast<
 
     let toRemoveId: string | undefined = undefined
     if (isError) {
-      toastRef.current.error({
-        title: props.toastMessage?.onError ?? (
-          <p>{t("pools.reviewTransaction.toast.error")}</p>
-        ),
-      })
+      if (error instanceof UnknownTransactionState) {
+        toastRef.current.unknown({
+          title: props.toastMessage?.onError ?? (
+            <p>{t("pools.reviewTransaction.toast.unknown")}</p>
+          ),
+        })
+      } else {
+        toastRef.current.error({
+          title: props.toastMessage?.onError ?? (
+            <p>{t("pools.reviewTransaction.toast.error")}</p>
+          ),
+        })
+      }
     }
 
     if (isLoading) {
@@ -65,7 +74,7 @@ export function ReviewTransactionToast<
     return () => {
       if (toRemoveId) toastRef.current.remove(toRemoveId)
     }
-  }, [t, props.toastMessage, isError, isSuccess, isLoading, props.link])
+  }, [t, props.toastMessage, isError, error, isSuccess, isLoading, props.link])
 
   return null
 }
